c2a3bb0c9fb9f29aed6a18ebafd6f4267adb5d8b
donkere-vader <donkere.v@gmail.com>
2021-01-28 10:58:36 +0100

    Made some css components for use in frontend

--- coder_hub/frontend/src/App.js
+++ coder_hub/frontend/src/App.js
@@ -1,5 +1,7 @@
 import './static/css/app.css';
-import './static/css/components.css';
+import './static/css/components/form.css';
+import './static/css/components/card.css';
+
 
 function App() {
   return (
@@ -8,8 +10,15 @@ function App() {
         <h3>Login</h3>
         <div style={{height: "10px"}}></div>
         <form>
-          <label htmlFor="nameinput">Name</label>
+          <label htmlFor="name_input">Name</label>
           <input type="text" name="name" id="name_input" placeholder="name" />
+          <div style={{height: "10px"}}></div>
+
+          <label htmlFor="password_input">Password</label>
+          <input type="password" name="name" id="password_input" placeholder="password" />
+          <div style={{height: "10px"}}></div>
+
+          <input className="submit button" type="submit" value="Login"/>
         </form>
       </div>
     </div>
