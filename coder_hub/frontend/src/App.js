daef850830935a2a16d295761599af5940116192
monyorojoseph <monyorojoseph@gmail.com>
2021-01-29 04:56:00 +0300

    updated frontend folder
    added navbar and changed sing in page style

--- coder_hub/frontend/src/App.js
+++ coder_hub/frontend/src/App.js
@@ -1,26 +1,13 @@
-import './static/css/app.css';
-import './static/css/components/form.css';
-import './static/css/components/card.css';
+
+import Header from './components/Header'
+import Login from './components/Login'
 
 
 function App() {
   return (
     <div className="App">
-      <div className="card" style={{width: "500px"}}>
-        <h3>Login</h3>
-        <div style={{height: "10px"}}></div>
-        <form>
-          <label htmlFor="name_input">Name</label>
-          <input type="text" name="name" id="name_input" placeholder="name" />
-          <div style={{height: "10px"}}></div>
-
-          <label htmlFor="password_input">Password</label>
-          <input type="password" name="name" id="password_input" placeholder="password" />
-          <div style={{height: "10px"}}></div>
-
-          <input className="submit button" type="submit" value="Login"/>
-        </form>
-      </div>
+      <Header />
+      {/* <Login /> */}
     </div>
   );
 }
---
+++ coder_hub/frontend/src/components/Login.js
@@ -0,0 +1,32 @@
+import { FaGithub, FaGoogle } from 'react-icons/fa';
+
+const Login = () => {
+    return (        
+        <div className="sign-in-container">
+            <h3>Sign In</h3>
+            <form method="POST" className="form">
+                <div className="form-field">                    
+                    <label className="label" htmlFor="name_input">Name</label>
+                    <input className="input" type="text" name="name" id="name_input" placeholder="name" />
+                </div>
+                <div className="form-field">
+                    <label className="label" htmlFor="password_input">Password</label>
+                    <input className="input" type="password" name="name" id="password_input" placeholder="password" />
+                </div>       
+
+                <input id="submit" className="btn" type="submit" value="Sign In"/>
+                <input className="btn btn-blue" type="button" value="Forgot Password"/>
+                <p>Don't have an account <a href="#">Register</a></p>
+
+            </form>
+            {/* third party accounts */}
+            <div className="third-party">
+                <p>Sign in with one of 3rd party accounts</p>
+                <button className="btn" id="google"><FaGoogle /> Sign in with Google</button>
+                <button className="btn" id="github"><FaGithub /> Sign in with Github</button>
+            </div>
+        </div>
+    )
+}
+
+export default Login
