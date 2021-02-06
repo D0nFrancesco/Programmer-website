b2e36c4cf181cb433cd2349d056d69695e768040
donkere-vader <donkere.v@gmail.com>
2021-02-06 16:36:05 +0100

    Added admin page to /api/ route

--- coder_hub/api/admin.py
+++ coder_hub/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Post, Tag, Comment, Project, Version
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Tag)
+admin.site.register(Comment)
+admin.site.register(Project)
+admin.site.register(Version)
--- coder_hub/api/urls.py
+++ coder_hub/api/urls.py
@@ -21,6 +21,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html")),
-    
-    
+    path('admin/', admin.site.urls),
 ]
