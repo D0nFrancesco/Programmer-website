8a6b617b8b2fb05bc194c543d7c7f8f0f2e92898
donkerevader <ctweeduizendvier@yahoo.com>
2021-02-27 22:50:01 +0100

    Start on project page; API calls working

--- coder_hub/frontend/src/pages/project.js
+++ coder_hub/frontend/src/pages/project.js
@@ -1,16 +1,68 @@
+import $ from "jquery";
+import React from 'react';
 
 
-function Project({match}) {
-    return (
-        <div className="App">
-            <div className="center">
-                <div className="card">
-                    <h3>Project page</h3>
-                    <span>{ match.params.projectid }</span>
+class Project extends React.Component {
+    constructor(props) {
+        super(props);
+        this.project_id = props.match.params.projectid;
+
+        this.state = {
+            postTitle: 'Loading...',
+            postText: 'Loading...',
+            postAuthor: 'Loading...',
+            postUpvotes: 0,
+            postDownvotes: 0,
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="center">
+                    <div className="card">
+                        <div>
+                            <h3>{ this.state.postTitle }</h3>
+                            <span>{ this.state.postAuthor }</span>
+                        </div>
+                        <span>{ this.state.postText }</span>
+                        <div className="post-bar">
+                            <div>{ this.state.postUpvotes }</div>
+                            <div>{ this.state.postDownvotes }</div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        );
+    }
+
+    componentDidMount() {
+        // Get API info
+        var component = this;
+
+        // post data
+        $.get(
+            `http://localhost:8000/api/post/${ this.project_id }`
+        ).done(function(data) {
+            console.log(data);
+            component.setState({
+                postTitle: data.title,
+                postText: data.text,
+                postUpvotes: data.upvoted_by.len ? data.upvoted_by.len : 0,
+                postDownvotes: data.downvoted_by.len ? data.downvoted_by.len : 0,
+            });
+
+            $.get(
+                `http://localhost:8000/api/user/${ data.user }`
+            ).done(function(data) {
+                component.setState({
+                    postAuthor: data.username
+                });
+            })
+        });
+
+        
+    }
 }
 
 export default Project;
